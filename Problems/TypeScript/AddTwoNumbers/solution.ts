bd7f2e8c33d96e82f5b2664fea1622085fedbded
elijah0039 <80763794+RadGoose@users.noreply.github.com>
2022-04-16 15:58:35 +0200

    Worked shortly with AddTwoNumbers
    Implemented a class instance for a different take on the problem.

--- Problems/TypeScript/AddTwoNumbers/solution.ts
+++ Problems/TypeScript/AddTwoNumbers/solution.ts
@@ -8,13 +8,39 @@ class ListNode {
         }
 }
 
-function addTwoNumbers(nodeOne: ListNode | null, nodeTwo: ListNode | null): ListNode | null {
-    let termOne = nodeOne, termTwo = nodeTwo;
-    let sum = new ListNode();
-    
-    
-    while (termOne || termTwo) {
+class AddTwoNumbersInstance {
+    termOne: ListNode | null
+    termTwo: ListNode | null
+    output = new ListNode(0)
+    sum = 0
+    carry = 0
+
+    step () {
+        this.sum = getListNodeValue(this.termOne) + getListNodeValue(this.termTwo)
         
     }
-    return 
-};
+
+    constructor(termOne?: ListNode | null, termTwo?: ListNode | null) {
+        this.termOne = termOne
+        this.termTwo = termTwo
+    }
+}
+
+function addTwoNumbers(nodeOne: ListNode | null, nodeTwo: ListNode | null): ListNode | null {
+    let workingInstance = new AddTwoNumbersInstance(nodeOne, nodeTwo)
+    
+    while (workingInstance.termOne || workingInstance.termTwo || workingInstance.sum > 0) {
+        
+    }
+
+    return workingInstance.output
+}
+
+
+function getListNodeValue(node: ListNode | null): number {
+    return node && node.val ? node.val : 0
+}
+
+function getNextListNode(node: ListNode | null): ListNode {
+    return node && node.next ? node.next : null
+}
