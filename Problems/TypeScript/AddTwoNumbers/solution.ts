ba0dda5b9526e8eb92e43aedf317f4141b0b63b9
elijah0039 <80763794+RadGoose@users.noreply.github.com>
2022-04-18 20:50:59 +0200

    Short session: Worked on first solution. Currently giving correct answers but in reversed order.

--- Problems/TypeScript/AddTwoNumbers/solution.ts
+++ Problems/TypeScript/AddTwoNumbers/solution.ts
@@ -1,23 +1,21 @@
-class ListNode {
-    val: number
-    next: ListNode | null
-    constructor(val?: number, next?:
-        ListNode | null) {
-            this.val = (val === undefined ? 0 : val)
-            this.next = (next === undefined ? null : next)
-        }
-}
+import { ListNode } from "./listNodeClass"
 
 class AddTwoNumbersInstance {
     termOne: ListNode | null
     termTwo: ListNode | null
-    output = new ListNode(0)
+    output: ListNode
     sum = 0
     carry = 0
 
     step () {
-        this.sum = getListNodeValue(this.termOne) + getListNodeValue(this.termTwo)
-        
+        this.sum = getNodeValue(this.termOne) + getNodeValue(this.termTwo) + this.carry
+
+        this.termOne = getNextNode(this.termOne)
+        this.termTwo = getNextNode(this.termTwo)
+
+        this.carry = this.sum >= 10 ? 1 : 0 
+
+        this.output = new ListNode(this.sum % 10, this.output)
     }
 
     constructor(termOne?: ListNode | null, termTwo?: ListNode | null) {
@@ -29,18 +27,18 @@ class AddTwoNumbersInstance {
 function addTwoNumbers(nodeOne: ListNode | null, nodeTwo: ListNode | null): ListNode | null {
     let workingInstance = new AddTwoNumbersInstance(nodeOne, nodeTwo)
     
-    while (workingInstance.termOne || workingInstance.termTwo || workingInstance.sum > 0) {
-        
+    while (workingInstance.termOne || workingInstance.termTwo) {
+        workingInstance.step()
     }
 
     return workingInstance.output
 }
 
 
-function getListNodeValue(node: ListNode | null): number {
+function getNodeValue(node: ListNode | null): number {
     return node && node.val ? node.val : 0
 }
 
-function getNextListNode(node: ListNode | null): ListNode {
+function getNextNode(node: ListNode | null): ListNode {
     return node && node.next ? node.next : null
 }
