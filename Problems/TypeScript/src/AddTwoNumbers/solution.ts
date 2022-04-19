54e475eb4ed20ed3b236a00294b9df61e1e7465a
elijah0039 <80763794+RadGoose@users.noreply.github.com>
2022-04-19 21:51:31 +0200

    AddTwoNumbers update
    Added two helper functions to the solution.ts module. Last work for the evening!

--- Problems/TypeScript/src/AddTwoNumbers/solution.ts
+++ Problems/TypeScript/src/AddTwoNumbers/solution.ts
@@ -9,3 +9,11 @@ export function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNod
 
     return output
 };
+
+export function getNodeVal(node: ListNode | null): number | null {
+    return node ? node.val : null
+}
+
+export function getNextNode(node: ListNode | null): ListNode | null {
+    return node && node.next ? node.next : null
+}
