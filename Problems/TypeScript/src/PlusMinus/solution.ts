fc7654965c2fe0b2f6ba8a075a63dbb592f6ffcc
elijah0039 <80763794+RadGoose@users.noreply.github.com>
2022-05-19 17:41:49 +0200

    PlusMinus started
    From HackerRank

---
+++ Problems/TypeScript/src/PlusMinus/solution.ts
@@ -0,0 +1,10 @@
+export function plusMinus(arr: number[]): string[] {
+  let pos = 0;
+  let neg = 0;
+  let zer = 0;
+
+  for (const num of arr) {
+  }
+
+  return [pos.toFixed(6), neg.toFixed(6), zer.toFixed(6)];
+}
---
+++ Problems/TypeScript/tests/plusMinus.test.ts
@@ -0,0 +1,6 @@
+import { expect } from "chai";
+import { plusMinus } from "../src/PlusMinus/solution";
+
+describe("plusMinus", () => {
+  it("should return ");
+});
